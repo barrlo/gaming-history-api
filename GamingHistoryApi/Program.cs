60c85c4bba6d8addc964d5437c35c0dd4748c74a
barrlo <dist.runner3@gmail.com>
2024-03-16 15:20:53 -0500

    add cors policy

--- GamingHistoryApi/Program.cs
+++ GamingHistoryApi/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://gaming-history.barrlo.net"); // , "http://localhost:3000");
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -10,14 +18,24 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
 
+// var  localOrigins = "LocalOrigins";
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(localOrigins, policy =>
+//     {
+//         policy.WithOrigins("https://localhost:3000");
+//     });
+// });
+// app.UseCors(localOrigins);
+// }
+
+app.UseCors();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
